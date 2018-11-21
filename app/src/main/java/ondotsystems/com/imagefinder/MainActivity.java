4751dc620db3c119ca4b11a0a7ad2d3269d6e6d0
Harika Konagala <harika_konagala@Georges-MacBook-Pro-2.local>
2018-11-21 14:28:18 -0500

    fingerprint auth

--- app/src/main/java/ondotsystems/com/imagefinder/MainActivity.java
+++ app/src/main/java/ondotsystems/com/imagefinder/MainActivity.java
@@ -1,23 +1,128 @@
 package ondotsystems.com.imagefinder;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.marcoscg.fingerauth.FingerAuth;
+import com.marcoscg.fingerauth.FingerAuthDialog;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //TODO
-    //1. xml for two screens editext and recyclerview with grid layout
-    //2. volley & okhttp for rest API - async task
-    //3. fingerprint - if(hardware syupported) move forward
+    //1. fingerprint auth - if(hardware syupported) move forward
+    //2. xml for two screens editext and recyclerview with grid layout
+    //3. volley & okhttp for rest API - async task
+
     //4. scroll view to the grid
     //5. picasso libr for image parsing
     //6. code structure & readability
     //7. design patterns
     //8. handle orientation changes & edge cases
 
+    private FingerAuthDialog fingerAuthDialog;
+    boolean hasFingerprintSupport;
+
+    Button authenticateUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        authenticateUser = findViewById(R.id.fingerprint_dialog);
+
+        hasFingerprintSupport = FingerAuth.hasFingerprintSupport(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //register click listener
+        authenticateUser.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //unregister click listener
+        authenticateUser.setOnClickListener(null);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if(id == R.id.fingerprint_dialog) {
+            fingerAuthDialog = null;
+            //Show dialog only on devices that support fingerprint auth
+            if(hasFingerprintSupport) {
+                createFingerprintDialog();
+            } else {
+                //just for the sake of assignment, creating a work around for fingerprint auth
+                // but ideally should ask for alternate password/pin
+                createContinueDialog();
+            }
+        }
+
+    }
+
+    private void createFingerprintDialog() {
+        fingerAuthDialog = new FingerAuthDialog(this)
+                .setTitle(getString(R.string.signin))
+                .setCancelable(false)
+                .setMaxFailedCount(3)
+                .setNegativeButton(getResources().getString(R.string.cancel), null)
+                .setOnFingerAuthListener(new FingerAuth.OnFingerAuthListener() {
+                    @Override
+                    public void onSuccess() {
+                        openSearchActivity();
+                    }
+
+                    @Override
+                    public void onFailure() {
+                        //do nothing; handled by fingerprint auth lib
+                    }
+
+                    @Override
+                    public void onError() {
+                        //do nothing; handled by fingerprint auth lib
+                    }
+                });
+
+        fingerAuthDialog.show();
+    }
+
+
+
+    private void createContinueDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.fingerprint_error))
+                .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        openSearchActivity();
+                    }
+                })
+                .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void openSearchActivity() {
+        //go to search screen
+        Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+        startActivity(intent);
     }
 }
