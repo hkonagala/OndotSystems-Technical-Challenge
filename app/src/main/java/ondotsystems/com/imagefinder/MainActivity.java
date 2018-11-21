c8cc28a300ac028c4f6a93ee93eea44c335c4d24
Harika Konagala <harika_konagala@Georges-MacBook-Pro-2.local>
2018-11-21 13:23:55 -0500

    .gitignore update

---
+++ app/src/main/java/ondotsystems/com/imagefinder/MainActivity.java
@@ -0,0 +1,23 @@
+package ondotsystems.com.imagefinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO
+    //1. xml for two screens editext and recyclerview with grid layout
+    //2. volley & okhttp for rest API - async task
+    //3. fingerprint - if(hardware syupported) move forward
+    //4. scroll view to the grid
+    //5. picasso libr for image parsing
+    //6. code structure & readability
+    //7. design patterns
+    //8. handle orientation changes & edge cases
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
